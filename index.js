716fc6db65d834e04bfa8206076af32f49b360cc
Patrick Cavanaugh <pcavanaugh@vistaprint.com>
2015-01-23 12:59:38 -0500

    set window._jsonp_callbacks only when invoked
    this is to attempt to avoid the error:
    
        window._jsonp_callbacks = {}
        ^
        ReferenceError: window is not defined
        at Object.<anonymous> (/Users/patrick/src/tower/node_modules/dlite-jsonp/index.js:14:1)
        at Module._compile (module.js:456:26)
        at Object.Module._extensions..js (module.js:474:10)
        at Module.load (module.js:356:32)
        at Function.Module._load (module.js:312:12)
        at Module.require (module.js:364:17)
        at require (module.js:380:17)
        at Object.<anonymous> (/Users/patrick/src/tower/dist/mason/script/pips/podcast.js:3:13)
        at Module._compile (module.js:456:26)
        at Object.Module._extensions..js (module.js:474:10)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = function(url, cb) {
   var id = 'j' + (Math.random() * (1<<30)).toString(16).replace('.', '')
     , script = document.createElement('script')
 
+  window._jsonp_callbacks = window._jsonp_callbacks || {};
   window._jsonp_callbacks[id] = function(res) {
     cb && cb(res)
     delete window._jsonp_callbacks[id]
@@ -11,4 +12,3 @@ module.exports = function(url, cb) {
   script.src = url.replace('callback=%3F', 'callback=_jsonp_callbacks.' + id)
   document.getElementsByTagName('head')[0].appendChild(script)
 }
-window._jsonp_callbacks = {}
